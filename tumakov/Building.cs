a0cfd530875a4a90b3d4a55164ce391d4a452e7c
Роман Пигуляк <romapiguliak@gmail.com>
2024-12-05 21:11:51 +0300

    Добавьте файлы проекта.

---
+++ tumakov/Building.cs
@@ -0,0 +1,77 @@
+﻿using System;
+namespace Tumakov
+{
+    public class Building
+    {
+        private int number;
+        private double height;
+        private int floors;
+        private int apartments;
+        private int entrances;
+        private static int lastNumber = 0;
+        public Building(double height = 0, int floors = 0, int apartments = 0, int entrances = 0)
+        {
+            Number = LastUsedNumber();
+            Height = height;
+            Floors = floors;
+            Apartments = apartments;
+            Entrances = entrances;
+            if (number <= 0 | height <= 0 | floors <= 0 | apartments <= 0 | entrances <= 0 | apartments % entrances != 0 | apartments % floors != 0)
+            {
+                throw new ArgumentException();
+            } 
+        }
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        public double Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public int Floors
+        {
+            get { return floors; }
+            set { floors = value; }
+        }
+        public int Apartments
+        {
+            get { return apartments; }
+            set { apartments = value; }
+        }
+        public int Entrances
+        {
+            get { return entrances; }
+            set { entrances = value; }
+        }
+        public double FloorHeight()
+        {
+            return height / floors;
+        }
+        public int ApartmentsPerEntranceCount()
+        {
+            return apartments / entrances;
+        }
+        public int ApartmentsPerFloorCount()
+        {
+            return apartments / floors;
+        }
+        private static int LastUsedNumber()
+        {
+            return ++lastNumber;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Номер здания: {number}");
+            Console.WriteLine($"Высота здания: {height} м");
+            Console.WriteLine($"Количество этажей: {floors}");
+            Console.WriteLine($"Количество квартир: {apartments}");
+            Console.WriteLine($"Количество подъездов: {entrances}");
+            Console.WriteLine($"Высота этажа: {FloorHeight():F2} м");
+            Console.WriteLine($"Количество квартир на этаже: {ApartmentsPerFloorCount()}");
+            Console.WriteLine($"Количество квартир в подъезде: {ApartmentsPerEntranceCount()}");
+        }
+    }
+}
---
+++ tumakov/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace Tumakov
+{
+    class Laba
+    {
+        static void Main()
+        {
+            Task1();
+            Task2();
+            Task3();
+            Task4();
+        }
+        static void Task1()
+        {
+            Console.WriteLine("7.1");
+            Bank account1 = new Bank();
+            account1.Balance = 5551111;
+            account1.AccountNumber = 134567890;
+            account1.AccountType = BankAccountType.current;
+            account1.Print();
+        }
+        static void Task2()
+        {
+            Console.WriteLine("7.2");
+            Bank account2 = new Bank();
+            account2.Balance = 1000;
+            account2.AccountType = BankAccountType.savings;
+            account2.Print();
+        }
+        static void Task3()
+        {
+            try
+            {
+                Console.WriteLine("7.3");
+                Bank account3 = new Bank();
+                Console.WriteLine("Введите баланс:");
+                account3.Balance = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("Сколько снять?");
+                account3.Withdraw(decimal.Parse(Console.ReadLine()));
+                Console.WriteLine("Сколько положить?");
+                account3.Deposit(decimal.Parse(Console.ReadLine()));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный ввод");
+            }
+        }
+        static void Task4()
+        {
+            try {
+                Console.WriteLine("dz7.1");
+                Building building = new Building(100, 30, 150, 5);
+                building.Print();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Неверный ввод");
+            }
+        }
+    }
+}
